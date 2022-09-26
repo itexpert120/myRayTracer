e168d2f1ef4d980d30f1907724be2cc0e7c55d5b
Zeeshan Ahmad <itexpert120@gmail.com>
2022-09-26 16:48:27 +0500

    Add Changes

--- main.cpp
+++ main.cpp
@@ -9,7 +9,9 @@
 #include "camera.h"
 #include "material.h"
 
+// C++ Libraries
 #include <iostream>
+#include <fstream>
 
 color ray_color(const ray& r, const hittable& world, int depth) {
 	hit_record rec;
@@ -34,7 +36,7 @@ color ray_color(const ray& r, const hittable& world, int depth) {
 hittable_list random_scene() {
 	hittable_list world;
 
-	// add groudn
+	// add ground
 	auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
 	world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
 
@@ -103,8 +105,12 @@ int main() {
 
 	camera cam(lookfrom, lookat, vup, fov, aspect_ratio, aperture, dist_to_focus);
 
+	// fstream
+	std::ofstream output;
+	output.open("C:\\Users\\Random Guy\\source\\repos\\myRayTracer\\x64\\Debug\\render.ppm");
+
 	//render
-	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+	output << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
 	for (int j = image_height - 1; j >= 0; j--) {
 		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
@@ -116,8 +122,9 @@ int main() {
 				ray r = cam.get_ray(u, v);
 				pixel_color += ray_color(r, world, max_depth);
 			}
-			write_color(std::cout, pixel_color, samples_per_pixel);
+			write_color(output, pixel_color, samples_per_pixel);
 		}
+		output << '\n';
 	}
 	std::cerr << "\nDone.\n";
 
